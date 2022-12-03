9e02efc5f6c13dfaf64b0025021e8871c887917c
Richard Wendel <richard.wendel@hey.com>
2022-12-03 12:18:09 -0500

    day 3

--- src/bin/3.rs
+++ src/bin/3.rs
@@ -1,67 +1,91 @@
+#![feature(array_chunks)]
+use std::convert::TryInto;
+
 use itertools::Itertools;
-use std::collections::HashSet;
+
 fn main() {
     let sample = include_str!("../../samples/3.txt");
+    let sample_2 = include_str!("../../samples/3_2.txt");
     let input = include_str!("../../inputs/3.txt");
-    let part_one = aoc::ProblemSolution::new(&part_one, 157);
-    let part_two = aoc::ProblemSolution::new(&part_two, 70);
-    aoc::run_all(part_one, part_two, sample, input);
+    let input_2 = include_str!("../../inputs/3_2.txt");
+    let part_one = aoc::ProblemSolution::new(&part_one, 157 /* TODO fill in */);
+    let part_two = aoc::ProblemSolution::new(&part_two, 70 /* TODO fill in */);
+    aoc::run_all(part_one, part_two, sample, input, sample_2, input_2);
 }
 
-fn find_common(line: &str) -> char {
-    let (left_compartment, right_compartment) = line.split_at(line.len() / 2);
-    let left_set = left_compartment.chars().collect::<HashSet<_>>();
-    let right_set = right_compartment.chars().collect::<HashSet<_>>();
-    let mut intersect = left_set.intersection(&right_set);
-    if intersect.clone().count() != 1 {
-        println!(
-            "Intersection between {} and {} is {:?}",
-            left_compartment, right_compartment, intersect
-        );
-        panic!("Intersection is not 1");
-    }
-
-    *intersect.next().unwrap()
-}
-
-fn compute_priority(c: char) -> usize {
-    if c.is_uppercase() {
-        c as usize - 'A' as usize + 27
+fn to_priority(letter: &char) -> usize {
+    let p = *letter as usize;
+    if p >= 97 {
+        p - 96
     } else {
-        c as usize - 'a' as usize + 1
+        p - 38
     }
 }
 
 fn part_one(lines: &[&str]) -> usize {
+    assert!(to_priority(&"A".chars().nth(0).unwrap()) == 27);
+    assert!(to_priority(&"L".chars().nth(0).unwrap()) == 38);
+    assert!(to_priority(&"a".chars().nth(0).unwrap()) == 1);
     lines
         .iter()
-        .map(|line| compute_priority(find_common(line)))
+        .map(|line| {
+            let length = line.len();
+            assert!(length % 2 == 0);
+            let chars_priority = line.chars().map(|c| to_priority(&c));
+            let mut check_against: [i32; 53] = [0; 53];
+            let mut found = None;
+            chars_priority.enumerate().for_each(|(i, x)| {
+                if i < length / 2 {
+                    // First half
+                    check_against[x] = 1;
+                } else {
+                    // second half
+                    if check_against[x] == 1 {
+                        if found.is_none() {
+                            found = Some(x)
+                        } else {
+                            assert!(found.unwrap() == x)
+                        }
+                    }
+                }
+            });
+            assert!(found.is_some());
+
+            found.unwrap()
+        })
         .sum()
 }
 
 fn part_two(lines: &[&str]) -> usize {
-    lines
-        .chunks(3)
-        .map(|group| {
-            let common_item = group
-                .iter()
-                .map(|line| line.chars().collect::<HashSet<_>>())
-                .fold(None, |acc, set| match acc {
-                    None => Some(set),
-                    Some(acc_set) => {
-                        Some(acc_set.intersection(&set).cloned().collect::<HashSet<_>>())
-                    }
+    assert!(lines.len() % 3 == 0);
+    lines.array_chunks::<3>()
+        .map(|line_chunk| {
+            let mut check_against: [u32; 53] = [0; 53];
+            let mut found = None;
+            line_chunk.into_iter().enumerate().for_each(|(i, line)|{
+                println!("on line {}", line);
+                let chars_priority = line.chars().map(|c| to_priority(&c));
+                chars_priority.for_each(|x| {
+                        assert!(i < 3);
+                        if x == 18 {
+                            println!("on r, check_against: {}", check_against[x]);
+                        }
+                        check_against[x] = check_against[x] | (1 << (i ));
+                        assert!(7 == 0b111u32);
+                        if check_against[x] == 0b111u32 {
+                            if found.is_none() {
+                                found = Some(x)
+                            } else {
+                                assert!(found.unwrap() == x)
+                            }
+                        }
+                        if x == 18 {
+                            println!("on r, after check_against: {}", check_against[x]);
+                        }
+                    });
                 });
-            match common_item {
-                None => panic!("No common item"),
-                Some(set) => {
-                    if set.len() != 1 {
-                        println!("Not exactly 1 common item: {:?}", set);
-                        panic!();
-                    }
-                    compute_priority(*set.iter().next().unwrap())
-                }
-            }
-        })
-        .sum::<usize>()
+                assert!(found.is_some());
+
+                found.unwrap()
+            }).sum()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -26,28 +26,21 @@ fn get_lines(file: &str) -> Vec<&str> {
     file.lines().collect_vec()
 }
 
-pub fn run_all(part_one: ProblemSolution, part_two: ProblemSolution, sample: &str, input: &str) {
+pub fn run_all(part_one: ProblemSolution, part_two: ProblemSolution, sample: &str, input: &str, sample_2: &str, input_2: &str) {
     let sample = get_lines(sample);
     let real = get_lines(input);
+    let result = part_one.run(&sample);
+    assert_eq!(result, part_one.sample_solution);
+    let start = Instant::now();
+    let result = part_one.run(&real);
+    println!("Part one: {:?}, took {:?}", result, start.elapsed());
 
-    thread::scope(|s| {
-        s.spawn(|| {
-            let result = part_one.run(&sample);
-            assert_eq!(result, part_one.sample_solution);
-        });
-        s.spawn(|| {
-            let start = Instant::now();
-            let result = part_one.run(&real);
-            println!("Part one: {:?}, took {:?}", result, start.elapsed());
-        });
-        s.spawn(|| {
-            let result = part_two.run(&sample);
-            assert_eq!(result, part_two.sample_solution);
-        });
-        s.spawn(|| {
-            let start = Instant::now();
-            let result = part_two.run(&real);
-            println!("Part two: {:?}, took {:?}", result, start.elapsed());
-        });
-    });
+    let sample_2 = get_lines(sample_2);
+    let real_2 = get_lines(input_2);
+    let result = part_two.run(&sample_2);
+    assert_eq!(result, part_two.sample_solution);
+    let start = Instant::now();
+    let result = part_two.run(&real_2);
+    println!("Part one: {:?}, took {:?}", result, start.elapsed());
+
 }
